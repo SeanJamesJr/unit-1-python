06936df9f64d8db9bb2831e859f8cfa665d403a3
Sean James Jr <sjamesjr@brooklynsteamcenter.org>
2023-09-19 09:50:36 -0400

    hi

---
+++ benchmark.py
@@ -0,0 +1,39 @@
+#Excercise 1: Display your name in the console
+print("Sean")
+
+#Excercise 2: Create a variable called favorite show 
+# set it equal to your favirote show
+
+FavoriteShow="Spongebob"
+
+
+#exercise 3:
+
+height=5'7
+age=16
+
+
+#Exercise 4
+
+print("Im going to break")
+
+#exercise 5
+
+list(favoritefoods)={ kfc,popeyes, watermelon}
+
+#exercise 6 
+
+ if age
+
+#exercise 7
+
+print("what is your name?")
+input(+name)
+print(Hello +name )
+
+#exerciese 8
+
+
+#exercise 9
+
+while
