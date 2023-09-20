3bffeb0b223f059c295d39c9067a15b762a51026
Sean James Jr <sjamesjr@brooklynsteamcenter.org>
2023-09-20 10:51:40 -0400

    hi

---
+++ Data_Types.py
@@ -0,0 +1,35 @@
+#TASK 1: Create a float variable, and then convert it to an integer 
+#Print both the original variable and the converted integer.
+
+Float = 1.2
+myInteger=int(Float)
+print(Float)
+print(myInteger)
+
+#TASK 2: Write code that takes a number as input and prints whether 
+#it's positive, negative, or zero using if-elif-else statements.
+
+number=input("1")
+if number > 0 :
+ print("positive") 
+elif number < 0 :
+ print("negative")
+else :
+ print('zero')
+
+
+ #TASK 3: Write code that takes two numbers as input (an integer and a float), 
+#performs addition, subtraction, multiplication, and division, and prints the results.
+
+integer=input("")
+Float=input("")
+
+
+
+
+
+#TASK 4: Create a dictionary with keys as fruit names and values as their respective quantities. 
+#Then print out the quantity of one of the fruits.
+
+#TASK 5:Create a string variable with that is a list of numbers and convert that string into a tuple.
+#Then print out the both the original string and tuple.#
