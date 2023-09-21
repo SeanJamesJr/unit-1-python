3e1ca0b1e523299516c0eb8ef9026f44d7986157
Sean James Jr <sjamesjr@brooklynsteamcenter.org>
2023-09-21 10:49:11 -0400

    hi

---
+++ list_Blitz.py
@@ -0,0 +1,55 @@
+#Task 1: Create a list
+#Create a list with 4 elements and print it.
+
+Food=['Fried Chicken','koolaid','watermelon','Hot Sauce']
+print(Food)
+
+# I mad my variable then put all my elemntes in then  printed it and put food variable in parenthesees
+
+
+#Task 2: Add Element to a List
+#Add an element to the end of the list. Print the updated list.
+
+Food.append( "collard greens" )
+print(Food)
+
+# I did food.append because append is used to add something to alist and then i printed it to make the new list with the new element
+
+#Task 3: Remove Element from a List
+#Remove a specific element from the list. Print the updated list.
+
+
+Food.remove("koolaid")
+print(Food)
+
+# I did FOod.remove because the command remove takes a certain element of your choosing out of a list
+
+#Task 4: Modify Element in a List
+#Modify an element at a specific index with a new value. Print the updated list.
+
+print( Food[1] )
+Food[2] = "jerk chicken"
+print( Food )
+
+# I  used the index of hotsauce to change it to jerk chicken
+
+#Task 5: Append Multiple Elements to a List
+#Append multiple elements to the end of the list. Print the updated list.
+
+Food.append( "curry Chicken" )
+Food.append( "cornbead" )
+print(Food)
+
+# I repeated the appended command in to differnt lines because it would be an error if I added it at the same time 
+
+#Task 6: Delete Element at a Specific Index
+#Delete an element at a specific index. Print the updated list.
+
+del Food[1]
+print(Food)
+
+# I used the code del to take away a specifc elemnt in the index instead of using remove because i could 
+
+
+#Task 7: Create a List with a Range of Numbers
+#Create a list containing numbers within a specified range (inclusive). Print the created list
