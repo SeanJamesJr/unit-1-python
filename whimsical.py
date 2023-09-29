6e9289dc4c7c4846a0a8633fc6f0af4481186d07
Sean James Jr <sjamesjr@brooklynsteamcenter.org>
2023-09-29 10:51:58 -0400

    hi

--- whimsical.py
+++ whimsical.py
@@ -7,7 +7,7 @@ while i <= 10:
   print(i)
 i += 1
 
-
+# first i made a variable
 
 """
 2. Countdown:
@@ -19,15 +19,25 @@ while i >= 10:
   print(i)
   i -= 1
 
+# similar to1 i did a variable with 10 then in while loop kept talking 1 away and also printing it
 
 """
 3. Factorial Calculation:
 Write a program that calculates the factorial of a given number using a while loop.
 """
 
+product = 1
+fact = int(input("pick a integer"))
 
+while fact >1:
+  product = fact * product
+  fact -= 1 
+  print('product')
 
+  #
 """
+# let them put the inetegr then while the integer(fact) is greater than 1 its is multiplied by the product(1) then prints the product
+
 4. Password Guessing Game:
 Create a simple password guessing game using a while loop. Ask the user to guess a predefined password and provide appropriate feedback.
 """
@@ -39,14 +49,19 @@ while c != 'apples':
   else:
     print('try again')
 
+# first i made a empty variable then a while loop to guess one word until they get it right
 
 """
 5. Sum of Digits:
 Write a program that calculates the sum of the digits of a given number using a while loop.
 """
+a='Start'
+while a != 'Stop':
+   value = input('give a number')
+   value2= input("give me a second number")
+   print(int(value)+int(value2))
 
-
-
+#
 
 """
 6. Fibonacci Series:
