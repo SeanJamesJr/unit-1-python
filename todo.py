6918c9b730e9ac674bc1d4eb8e0b15e06e38684c
Sean James Jr <sjamesjr@brooklynsteamcenter.org>
2023-10-04 10:30:08 -0400

    hi

--- todo.py
+++ todo.py
@@ -1,25 +1,36 @@
 
 
 
-# make the list to hold ncurrent todos
+# make the list to hold current todos
 todos=['yo','ya','yen']
-# adds a item to the list
-addTodo=input(' enter word here to add Todo list')
-todos.append(addTodo)
-#  removes a item of choice from the list
-RemoveTodo=input('enter word here to remove from Todo list')
-todos.remove(input)
 
-print('your current todos are')
-print(todos)
-print(' would you like to add or remove a Todo?')
-decision=input('Add or remove')
-if (decision == 'Add'): 
-    addTodo=input(' What is your new todo')
-    todos.append(addTodo)
-elif (decision == 'remove'):
-    RemoveTodo=input('enter word here to remove from Todo list')
-    todos.remove(input)
-print('Your current todos are:')
-print(todos)
-print('would you like to add or remove a todo')
+
+while True:
+# states the items in the list  and aks if u want to add or remove one
+    print('your current todos are')
+    print()
+    print()
+    print(todos)
+    print()
+    print()
+    print(' would you like to add or remove a Todo?')
+    print()
+    decision=input('add or remove')
+    # adds a item to the list
+    if (decision == 'add'): 
+        addTodo=input(' What is your new todo ')
+        todos.append(addTodo)
+        
+        print()
+        print()
+        #  removes a item of choice from the list
+    elif (decision == 'remove'):
+        RemoveTodo=input('enter word here to remove from Todo list')
+        todos.remove(RemoveTodo)
+        print(todos)
+        print()
+        print()
+    print('Your current todos are:')
+    print(todos)
+    print()
+    print('would you like to add or remove a todo')
