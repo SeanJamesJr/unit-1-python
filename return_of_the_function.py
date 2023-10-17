1a4da51b2a180e4c3d193b3c77412176687a53df
Sean James Jr <sjamesjr@brooklynsteamcenter.org>
2023-10-17 16:55:56 -0400

    hi

--- return_of_the_function.py
+++ return_of_the_function.py
@@ -23,13 +23,26 @@ Task 3: Convert Temperature from Celsius to Fahrenheit:
 Write a function that takes a temperature in Celsius and returns 
 the equivalent temperature in Fahrenheit using the correct formula
 """
-def celtofar(C):
-  return  F = C *(5/2)+ 32
- convert = celtofar(10)
 
-#
+def temp(a): 
+    # Creates function
+    return (a * (9/5)) + 32 
+# returns value, multiples a by (9/5), and adds 32
+print(temp(1),"°C") 
+# Calls function
+
 """
 Task 4: Calculate the Average of Numbers:
 Write a function that takes a list of numbers 
 and returns the average (mean) of those numbers.
 """
+def avgNumber(a): 
+    # Creates function
+    total = sum(a) 
+    # Retrieves total of sum of list
+    avg = total / len(a) 
+    # Retieves avg by dividing total by length of avg
+    return avg 
+# Returns avg value
+    return total / len(a) 
+# Returns and retieves avg by dividing total by length of avg
